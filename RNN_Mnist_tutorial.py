a83fd32427342a680e7cbbb0a162a2747251949e
Tejas Krishna Reddy <tejas.krishnareddy1415@gmail.com>
2019-08-07 19:16:10 -0400

    Update RNN_Mnist_tutorial.py

--- RNN_Mnist_tutorial.py
+++ RNN_Mnist_tutorial.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM, CuDNNLSTM ## CuDNNLSTM is superfast and trains with very very high accuracy
 
-
+#################### Pre-Processing #################################
 mnist = tf.keras.datasets.mnist  # mnist is a dataset of 28x28 images of handwritten digits and their labels
 (x_train, y_train),(x_test, y_test) = mnist.load_data()  # unpacks images to x_train/x_test and labels to y_train/y_test
 
@@ -12,7 +12,10 @@ x_test = x_test/255.0
 print(x_train.shape)
 print(x_train[0].shape)
 
+############################# Building The Model #####################
 model = Sequential()
+
+### Uncomment the lines to see the results for an LSTM layer. Currently executing CuDNNLSTM network.
 
 # IF you are running with a GPU, try out the CuDNNLSTM layer type instead (don't pass an activation, tanh is required)
 #model.add(LSTM(128, input_shape=(x_train.shape[1:]), activation='relu', return_sequences=True))
@@ -30,6 +33,7 @@ model.add(Dropout(0.2))
 
 model.add(Dense(10, activation='softmax'))
 
+### Declare an Optimizer - Adam Optimizer works well
 opt = tf.keras.optimizers.Adam(lr=0.001, decay=1e-6)
 
 # Compile model
@@ -39,6 +43,7 @@ model.compile(
     metrics=['accuracy'],
 )
 
+########## CuDNNLSTM's could get 91% in 1 min 15 secs. Whereas normal LSTM networks took approx 15 mins to achieve 70% accuracy #######
 model.fit(x_train,
           y_train,
           epochs=3,
